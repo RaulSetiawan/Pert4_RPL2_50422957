a10007a152833f3b192dbe8ebfc7f09b81551779
RaulSetiawan <Raulsetiawan310304@gmail.com>
2025-11-29 11:12:20 +0700

    first commit

---
+++ src/main/java/com/mycompany/pert4_50422957/Pert4_50422957.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.pert4_50422957;
+
+/**
+ *
+ * @author Acer
+ */
+public class Pert4_50422957 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
